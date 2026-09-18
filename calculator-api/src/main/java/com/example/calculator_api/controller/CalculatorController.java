4422e4f11131e21ba65befbc910ab5eaf6e06851
Ramana0306 <m.ramanamurugan2006@gmail.com>
2026-09-18 22:45:20 +0530

    Frontend cors

--- calculator-api/src/main/java/com/example/calculator_api/controller/CalculatorController.java
+++ calculator-api/src/main/java/com/example/calculator_api/controller/CalculatorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/calculator")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://calculators-api-tau.vercel.app")
 public class CalculatorController {
 
     @PostMapping("/add")
